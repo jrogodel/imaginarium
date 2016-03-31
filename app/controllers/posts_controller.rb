0b041d7f526cd61a161d982ee9b595ab6f5d4ced
Jamie Godel <jamiegodel@yahoo.com>
2016-03-30 23:07:01 -0400

    Configure voting system

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
 	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
@@ -35,10 +35,20 @@ class PostsController < ApplicationController
 		end
 	end
 
-
 	def destroy
 		@post.destroy
 		redirect_to root_path
+	end
+
+	# Define methods for voting system
+	def upvote
+		@post.upvote_by current_user
+		redirect_to :back
+	end
+
+	def downvote
+		@post.downvote_from current_user
+		redirect_to :back
 	end
 
 	private
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,11 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
 
+
+	# Paperclip Gem - Change image sizes via styles hash
 	has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
+	# Voting system initializer
+	acts_as_votable
 end
